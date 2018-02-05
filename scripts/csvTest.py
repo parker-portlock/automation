092d4e7b47359102dc74650952b6e4b97475ae5c
Parker Portlock <pportlock@accruent.com>
2018-02-05 21:43:29 +0000

    combined csv with object entry

--- scripts/csvTest.py
+++ scripts/csvTest.py
@@ -1,5 +1,13 @@
 import csv
-with open('addr.csv', 'rb') as csvfile:
+line_number = 0
+column = 0
+with open('../files/addr.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in reader:
-        print '_'.join(row)
+    reader = list(reader)
+#prints hostname column
+for i in range(len(reader)):
+    print reader[i][1]
+
+#prints ip column
+for i in range(len(reader)):
+	print reader[i][0]
--- scripts/objectEntry.py
+++ scripts/objectEntry.py
@@ -1,53 +1,74 @@
+import csv
 file = open("pyASA.txt","a+")
 continueQuery = "y"
+line_number = 0
+column = 0
+
+
+#opens CSV
+with open('../files/addr.csv', 'rb') as csvfile:
+    				reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    				reader = list(reader)
+
+
+
 while (continueQuery == "y"):
 
 #Object name and type creation
-	objectName = raw_input ("Please enter object name: ")
+#	objectName = raw_input ("Please enter object name: ")
 	objectType = raw_input ("Please enter the object type (network, service) ")
 	
 #Network object creation
 	if objectType == "network":
 		netType = raw_input ("What type of network object? (host, subnet) ")
 		if netType == "host":
-			netHost = raw_input ("Please enter the IPv4 host address: ")
-			print "object", objectType, objectName, "\n", "host", netHost
-			print >>file, "object", objectType, objectName, "\n", "host", netHost
+			loadFile = raw_input ("Did you load the addr.csv in /files? (y/n) ")
+			if loadFile == "y":
+				for i in range(len(reader)):
+					print "object", objectType, reader[i][1], "\n", "host", reader[i][0]
+			else:
+				print "invalid input"
 			continueQuery = raw_input ("Do you have any more objects? (y/n) ")
 		elif netType == "subnet":
-			netSubnet = raw_input ("Please enter the subnet in network (space) mask form: ")
-			print "object", objectType, objectName, "\n", "subnet", netSubnet
-			print >>file, "object", objectType, objectName, "\n", "subnet", netSubnet, "\n"
-			continueQuery = raw_input ("Do you have any more objects? (y/n) ")
-		else:
-			print "Invalid Input"
-	
-	
-	#service object creation
-	elif objectType == "service":
-		servProto = raw_input ("What is the protocol? (tcp, udp)")
-		if servProto == "tcp":
-			portType = raw_input ("Source or Destination port? ")
-			if portType == "source":
-				portNumber = raw_input ("Please enter the source port number: ")
-				print "OUTPUT HERE"
-			elif portType == "destination":
-				portNumber = raw_input ("Please enter the destination port number: ")
-				print "OUTPUT HERE"
+			loadFile = raw_input ("Did you load the object.csv? (y/n) ")
+			if loadFile == "y":
+				for i in range(len(reader)):
+					print "object", objectType, reader[i][1], "\n", "subnet", reader[i][0], reader[i][2]
 			else:
-				print "Invalid Input"
-		elif servProto == "udp":
-			portType = raw_input ("Source or Destination port? ")
-			if portType == "source":
-				portNumber = raw_input ("Please enter the source port number: ")
-				print "OUTPUT HERE"
-			elif portType == "destination":
-				portNumber = raw_input ("Please enter the destination port number: ")
-				print "OUTPUT HERE"
-			else:
-				print "Invalid Input"
-		else:
-			print "Invalid Input"
-	else:
-		print "Invalid Input"
-print "Thank You! Your ASA script is ready under the filename pyASA.txt. Don't forget to Like and Subscribe!"
+				print "invalid input"
+
+			continueQuery = raw_input ("Do you have any more object files? (y/n) ")
+
+
+#		else:
+#			print "Invalid Input"
+#	
+#	
+#	#service object creation
+#	elif objectType == "service":
+#		servProto = raw_input ("What is the protocol? (tcp, udp)")
+#		if servProto == "tcp":
+#			portType = raw_input ("Source or Destination port? ")
+#			if portType == "source":
+#				portNumber = raw_input ("Please enter the source port number: ")
+#				print "OUTPUT HERE"
+#			elif portType == "destination":
+#				portNumber = raw_input ("Please enter the destination port number: ")
+#				print "OUTPUT HERE"
+#			else:
+#				print "Invalid Input"
+#		elif servProto == "udp":
+#			portType = raw_input ("Source or Destination port? ")
+#			if portType == "source":
+#				portNumber = raw_input ("Please enter the source port number: ")
+#				print "OUTPUT HERE"
+#			elif portType == "destination":
+#				portNumber = raw_input ("Please enter the destination port number: ")
+#				print "OUTPUT HERE"
+#			else:
+#				print "Invalid Input"
+#		else:
+#			print "Invalid Input"
+#	else:
+#		print "Invalid Input"
+print "Thank You! Your ASA script is ready under the filename pyASA.txt. Don't forget to Like and Subscribe!"#
