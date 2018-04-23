a59abca1b9a2f80033977a54269c26affc2212c6
Parker Portlock <pportlock@accruent.com>
2018-04-23 15:22:09 +0000

    objectEntry converted to py3

--- scripts/objectEntry.py
+++ scripts/objectEntry.py
@@ -10,7 +10,7 @@ loadFile = input ("Did you load the addr.csv in /files? (y/n) ")
 if loadFile == "y":
 
 	#opens CSV
-	with open('../files/addr.csv', 'rb') as csvAddr:
+	with open('../files/addr.csv', 'rt') as csvAddr:
     		readerAddr = csv.reader(csvAddr, delimiter=',', quotechar='|')
     		readerAddr = list(readerAddr)
 
@@ -31,13 +31,13 @@ while (continueQuery == "y"):
 		netType = input ("What type of network object? (host, subnet) ")
 		if netType == "host":
 			for i in range(len(readerAddr)):
-				print >> file, "object", objectType, readerAddr[i][1], "\n", "host", readerAddr[i][0]
+				print("object", objectType, readerAddr[i][1], "\n", "host", readerAddr[i][0], file=open("../output/pyObject.txt", "a"))
 		else:
 			print ("invalid input")
 		continueQuery = input ("Do you have any more objects? (y/n) ")
 	elif netType == "subnet":
 		for i in range(len(readerAddr)):
-			print >> file, "object", objectType, readerAddr[i][1], "\n", "subnet", readerAddr[i][0], readerAddr[i][2]
+			print("object", objectType, readerAddr[i][1], "\n", "subnet", readerAddr[i][0], readerAddr[i][2], file=open("../output/pyObject.txt", "a"))
 		continueQuery = input ("Do you have any more object files? (y/n) ")
 
 	else:
@@ -82,9 +82,9 @@ while (continueQuery == "y"):
 groupQuery = input("Would you like to create an object-group with these servers? (y/n) ")
 while groupQuery == "y":
 	groupName = input("Please enter a name for the object group starting with 'OG_': ")
-	print >> file, "object-group network", groupName
+	print("object-group network", groupName, file=open("../output/pyObject.txt","a"))
 	for i in range(len(readerAddr)):
-		print >> file, "network-object object", readerAddr[i][1]
+		print("network-object object", readerAddr[i][1], file=open("../output/pyObject.txt","a"))
 	groupQuery = input("Do you have any more groups? (y/n) ")
 
 print ("Thank You! Your ASA script is ready at /output/pyObject.txt. Don't forget to Like and Subscribe!")
