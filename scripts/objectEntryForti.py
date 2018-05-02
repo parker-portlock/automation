02059961feba93a010ba12ef2dc33c17d2f301f0
Parker Portlock <pportlock@accruent.com>
2018-05-02 19:31:31 +0000

    converted forti script to py3

--- scripts/objectEntryForti.py
+++ scripts/objectEntryForti.py
@@ -1,10 +1,11 @@
 import csv
+import sys
 file = open("pyFortiObject.txt","a+")
 continueQuery = "y"
 line_number = 0
 
 #opens CSV
-with open('../files/addr.csv', 'rb') as csvAddr:
+with open('../files/addr.csv', 'rt') as csvAddr:
     				readerAddr = csv.reader(csvAddr, delimiter=',', quotechar='|')
     				readerAddr = list(readerAddr)
 
@@ -13,14 +14,16 @@ with open('../files/addr.csv', 'rb') as csvAddr:
 while (continueQuery == "y"):
 
 #Object creation
-	loadFile = raw_input ("Did you load the addr.csv in /files? (y/n) ")
+	loadFile = input ("Did you load the addr.csv in /files? (y/n) ")
 	if loadFile == "y":
 		for i in range(len(readerAddr)):
-			print >> file, "edit", readerAddr[i][1], "\n", "set subnet", readerAddr[i][0] , readerAddr[i][2], "\n", "next"
+			print ("edit", readerAddr[i][1], "\n", "set subnet", readerAddr[i][0] , readerAddr[i][2], "\n", "next", file=open("../output/pyFortiObject.txt", "a"))
 	else:
-		print "invalid input"
-	continueQuery = raw_input ("Do you have any more objects? (y/n) ")
+		print ("invalid input")
+	continueQuery = input ("Do you have any more objects? (y/n) ")
 			
 
 
-print "Thank You! Your FortiOS script is ready under the filename pyFortiObject.txt. Don't forget to Like and Subscribe!"
+print ("Thank You! Your FortiOS script is ready under the filename pyFortiObject.txt. Don't forget to Like and Subscribe!")
+
+sys.exit()
