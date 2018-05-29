d2869407cef769bace0dfcdb2c166dba9099ead9
SSJB <p.portlock@gmail.com>
2018-05-29 15:59:43 +0000

    changes

--- scripts/objectGroup.py
+++ scripts/objectGroup.py
@@ -12,30 +12,40 @@ def CiscoGroup():
 		with open('../files/localObjects.csv', 'rt') as csvLoc:
 		   		localAddr = csv.reader(csvLoc, delimiter=',', quotechar='|')
 		   		localAddr = list(localAddr)
+
 		with open('../files/localObjects.csv', 'rt') as csvRem:
-		   		remoteAddr = csv.reader(csvRem, delimiter=',', quotechar='|')
-		  		remoteAddr = list(remoteAddr)
+			remoteAddr = csv.reader(csvRem, delimiter=',', quotechar='|')
+			remoteAddr = list(remoteAddr)
 
 		#creates object group names
 		localGroupName = input("Please enter the name for the local network group: ")
 		remoteGroupName = input("Please enter the name for the remote network group: ")
 
-		#sends back to master
-		return localGroupName
-		return remoteGroupName
+		
 
 		#writes local object-group output
-		print("object-group network", groupName, file=open("../output/pyObject.txt","a"))
 		for i in range(len(localAddr)):
 			print("network-object object", localAddr[i][1], file=open("../output/pyObject.txt","a"))
 		
 		#writes remote object-group output
-		print("object-group network", groupName, file=open("../output/pyObject.txt","a"))
 		for i in range(len(remoteAddr)):
 			print("network-object object", remoteAddr[i][1], file=open("../output/pyObject.txt","a"))
-			
+
+		#sends back to master
+		return localGroupName
+		return remoteGroupName
+
 	elif tunnelQuery == "n":
+		with open('../files/addr.csv', 'rt') as csvAddr:
+			readerAddr = csv.reader(csvAddr, delimiter=',', quotechar='|')
+			readerAddr = list(readerAddr)
+
 		groupName = input("Please enter a name for the object group: ")
+		
+		for i in range(len(readerAddr)):
+			print("network-object object", readerAddr[i][1], file=open("../output/pyObject.txt","a"))
+		
+		return groupName
 	else:
 		print ("Invalid input; exiting...")
 		sys.exit()
