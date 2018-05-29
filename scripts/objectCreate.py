e4622ca941886634bb7616a59ca7a200b6fffa12
SSJB <p.portlock@gmail.com>
2018-05-29 15:52:57 +0000

    changes

--- scripts/objectCreate.py
+++ scripts/objectCreate.py
@@ -64,3 +64,33 @@ def CiscoObject():
 			else:
 				print ("Invalid input; exiting...")
 				sys.exit()
+
+		elif tunnelObject == "n":
+			loadFile = input ("Did you load the localObjects.csv and remoteObjects.csv in /files? (y/n) ")
+			if loadFile == "y":
+
+#opens CSV
+				with open('../files/objects.csv', 'rt') as csvObj:
+			    		objAddr = csv.reader(csvObj, delimiter=',', quotechar='|')
+			    		objAddr = list(objAddr)
+
+			else:
+				print ("WTF are you doing here then? Exiting Program")
+				sys.exit()
+
+#creates objects		
+			objectType = input ("Please enter the object type (network, service) ")
+			if objectType == "network":
+				netType = input ("What type of network object? (host, subnet) ")
+				if netType == "host":
+					for i in range(len(objAddr)):
+						print("object", objectType, objAddr[i][1], "\n", "host", objAddr[i][0], file=open("../output/pyObject.txt", "a"))
+				else:
+					print ("invalid input")
+				continueQuery = input ("Do you have any more objects? (y/n) ")
+			elif netType == "subnet":
+				for i in range(len(localAddr)):
+					print("object", objectType, objAddr[i][1], "\n", "subnet", objAddr[i][0], objAddr[i][2], file=open("../output/pyObject.txt", "a"))
+			else:
+				print ("Invalid input; exiting...")
+				sys.exit()
--- scripts/objectGroup.py
+++ scripts/objectGroup.py
@@ -5,14 +5,37 @@ import csv
 #object group function
 def CiscoGroup():
 #create group for ipsec tunnel
-		tunnelQuery = input("Is this a part of a tunnel? (y/n) ")
-		if tunnelQuery == "y":
-			localGroupName = input("Please enter the name for the local network group: ")
-			remoteGroupName = input("Please enter the name for the remote network group: ")
-			return localGroupName
-			return remoteGroupName
-		elif tunnelQuery == "n":
-			groupName = input("Please enter a name for the object group: ")
-		else:
-			print ("Invalid input; exiting...")
-			sys.exit()
+	tunnelQuery = input("Is this a part of a tunnel? (y/n) ")
+	if tunnelQuery == "y":
+
+		#opens CSVs
+		with open('../files/localObjects.csv', 'rt') as csvLoc:
+		   		localAddr = csv.reader(csvLoc, delimiter=',', quotechar='|')
+		   		localAddr = list(localAddr)
+		with open('../files/localObjects.csv', 'rt') as csvRem:
+		   		remoteAddr = csv.reader(csvRem, delimiter=',', quotechar='|')
+		  		remoteAddr = list(remoteAddr)
+
+		#creates object group names
+		localGroupName = input("Please enter the name for the local network group: ")
+		remoteGroupName = input("Please enter the name for the remote network group: ")
+
+		#sends back to master
+		return localGroupName
+		return remoteGroupName
+
+		#writes local object-group output
+		print("object-group network", groupName, file=open("../output/pyObject.txt","a"))
+		for i in range(len(localAddr)):
+			print("network-object object", localAddr[i][1], file=open("../output/pyObject.txt","a"))
+		
+		#writes remote object-group output
+		print("object-group network", groupName, file=open("../output/pyObject.txt","a"))
+		for i in range(len(remoteAddr)):
+			print("network-object object", remoteAddr[i][1], file=open("../output/pyObject.txt","a"))
+			
+	elif tunnelQuery == "n":
+		groupName = input("Please enter a name for the object group: ")
+	else:
+		print ("Invalid input; exiting...")
+		sys.exit()
