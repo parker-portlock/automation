5420aab8098e6f89029c55b158cd6f474a31ed7b
SSJB <p.portlock@gmail.com>
2018-06-04 18:42:15 +0000

    aclCreate modifications

---
+++ scripts/aclCreate.py
@@ -0,0 +1,19 @@
+#extendedACL.py because we know da wae
+import sys
+import csv
+
+def CiscoCryptoACL():
+	with open('../files/encGroups.csv', 'rt') as csvGroupName:
+		groupNames = csv.reader(csvGroupName, delimiter=',', quotechar='|')
+		groupNames = list(groupNames)
+	
+	cmapACL = input("Please name your crypto map ACL: ")
+	print("Creating crytpo map ACL...")
+	for i in range(len(groupNames)):
+		print ("access-list", cmapACL, "permit ip object-group", groupNames[i][0], "object-group", groupNames[i][1], file=open("../output/ipsec.txt","a")
+			
+			
+def CiscoFilterACL():
+	filterACL = input("Please name your VPN filter ACL: ")
+	print("Creating VPN filter ACL...")
+	print ("access-list" filterACL, "deny any any", file=open("../output/ipsec.txt","a")
--- scripts/extendedACL.py
+++
@@ -1,5 +0,0 @@
-#extendedACL.py because we know da wae
-import sys
-import csv
-
-def CiscoCryptoACL():
