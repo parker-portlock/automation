67c92681833911dc82035d80a1335da58f2f7323
SSJB <p.portlock@gmail.com>
2018-06-22 17:30:59 +0000

    changes to the object creation script

--- scripts/objectGroup.py
+++ scripts/objectGroup.py
@@ -34,3 +34,14 @@ def CiscoTunGroup():
 	for i in range(len(remoteAddr)):
 		print("network-object object", remoteAddr[i][1], file=open("../output/ipsec.txt","a"))
 	print("exit", file=open("../output/ipsec.txt","a"))
+
+def CiscoGroup():
+	groupName = input("What's the name for the object group? ")
+	with open('../files/addr.csv', 'rt') as csvObj:
+    		address = csv.reader(csvObj, delimiter=',', quotechar='|')
+    		address = list(address)
+
+	print("object-group network", groupName, file=open("../output/Objects.txt","a"))
+	for i in range(len(address)):
+		print("network-object object", address[i][1], file=open("../output/Objects.txt","a"))
+	print("exit", file=open("../output/Objects.txt","a"))
---
+++ scripts/quickObject.py
@@ -0,0 +1,27 @@
+import sys
+import csv
+import objectGroup
+
+loadFile = input ("Did you load the  in /files? (y/n) ")
+
+if loadFile == "y":
+	with open('../files/addr.csv', 'rt') as csvObj:
+		address = csv.reader(csvObj, delimiter=',', quotechar='|')
+		address = list(address)
+
+	for i in range(len(address)):
+	    netType = address[i][2]
+	    if netType =='':
+	    	print("object network", address[i][1], "\n", "host", address[i][0], file=open("../output/Objects.txt", "a"))
+	    elif netType !='':
+	        print("object netowrk", address[i][1], "\n", "subnet", address[i][0], address[i][2], file=open("../output/Objects.txt", "a"))
+
+groupQ = input("Do you want these in a group? (y/n)")
+if groupQ == 'y':
+	groupName = input("What's the name for the object group? ")
+	print("object-group network", groupName, file=open("../output/Objects.txt","a"))
+	for i in range(len(address)):
+		print("network-object object", address[i][1], file=open("../output/Objects.txt","a"))
+	print("exit", file=open("../output/Objects.txt","a"))
+else:
+	sys.exit()
