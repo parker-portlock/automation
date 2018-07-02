0064d6979bab1ca81adfcd4d9e33e6e8bc2b2fa3
SSJB <p.portlock@gmail.com>
2018-07-02 20:53:13 +0000

    add license to ipsec.py

--- scripts/ipsec.py
+++ scripts/ipsec.py
@@ -1,3 +1,20 @@
+    #ipsec.py -> Quickly create Cisco ikev1 and ikev2 tunnels
+    #Copyright (C) 2018 Parker M. Portlock
+
+    #This program is free software: you can redistribute it and/or modify
+    #it under the terms of the GNU General Public License as published by
+    #the Free Software Foundation, either version 3 of the License, or
+    #(at your option) any later version.
+
+    #This program is distributed in the hope that it will be useful,
+    #but WITHOUT ANY WARRANTY; without even the implied warranty of
+    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    #GNU General Public License for more details.
+
+    #You should have received a copy of the GNU General Public License
+    #along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import objectCreate
 import objectGroup
 import sys
