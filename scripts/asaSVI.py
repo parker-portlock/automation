f0693c84e02780c36d5c74b8fe95c57e03b0f1d2
SSJB <p.portlock@gmail.com>
2018-07-10 15:46:25 +0000

    append license

--- scripts/asaSVI.py
+++ scripts/asaSVI.py
@@ -1,14 +1,29 @@
+    #asaSVI.py -> Quickly create Cisco ASA SVIs
+    #Copyright (C) 2018 Parker M. Portlock
+
+    #This program is free software: you can redistribute it and/or modify
+    #it under the terms of the GNU General Public License as published by
+    #the Free Software Foundation, either version 3 of the License, or
+    #(at your option) any later version.
+
+    #This program is distributed in the hope that it will be useful,
+    #but WITHOUT ANY WARRANTY; without even the implied warranty of
+    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    #GNU General Public License for more details.
+
+    #You should have received a copy of the GNU General Public License
+    #along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import sys
 import csv
 
 loadFile = input ("Did you load the interface.csv in /files? (y/n) ")
 
-
 if loadFile == "y":
     with open('../files/interface.csv', 'rt') as csvInt:
     	interface = csv.reader(csvInt, delimiter=',', quotechar='|')
-    	interface = list(interface)
-    
+    	interface = list(interface)   
     
     for i in range(len(interface)-1):
         intType = interface[i+1][0]
