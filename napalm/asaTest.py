c4c8a806baa2dddcbb6383a5752dc77ee0853fdc
SSJB <p.portlock@gmail.com>
2018-11-08 17:47:08 +0000

    pushing updates

--- napalm/asaTest.py
+++ napalm/asaTest.py
@@ -5,7 +5,7 @@
 
 from napalm import get_network_driver
 driver = get_network_driver("asa")
-device = driver(hostname="10.78.50.7", username="net_pportlock", password="[p3anutbutt3r!]", optional_args = {'port': 443})
+device = driver(hostname="1.1.1.1", username="REDACTED", password="REDACTED", optional_args = {'port': 443})
 device.open()
 facts = device.get_facts()
 print (facts)
